061350d16792ee25d63afa9247fc063c74b7093c
Bo Kyung <113874167+BOKYUNGGG@users.noreply.github.com>
2023-01-16 17:38:02 +0900

    init routing

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import Router from './components/Router';
+import firebase from './firebase';
+
+console.log(firebase)
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Router />
   </React.StrictMode>
 );
